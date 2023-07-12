3e04f193d13c5927b949c1fcb51adac4bf0645d6
VitaliyBolgarov <125477680+VitaliyBolgarov@users.noreply.github.com>
2023-07-12 11:16:34 +0300

    hw-v1.0

---
+++ src/cat_api.js
@@ -0,0 +1,24 @@
+const BASE_URL = "https://api.thecatapi.com/v1";
+const API_KEY = "live_hP6H204vpUdETEOaUdWpI7E2L2kyydQsb5iYRdJtdkjILdxbt3ZNy6JmQuLU9CwE";
+
+export function fetchBreeds() {
+    return fetch(
+        `${BASE_URL}/breeds`
+    ).then(resp => {
+        if (!resp.ok) {
+            throw new Error(resp.statusText)
+        }
+        return resp.json()
+    })
+}
+
+export function fetchCatByBreed(breedId) {
+    return fetch(
+        `${BASE_URL}/images/search?limit=10&breed_ids=${breedId}&api_key=${API_KEY}`
+    ).then(resp => {
+        if (!resp.ok) {
+            throw new Error(resp.statusText)
+        }
+        return (resp.json())
+    })
+}
